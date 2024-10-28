0c3507886f21901cd05843aabf6ed8d14a661a26
dmatos <dmatos88@gmail.com>
2024-10-28 19:55:21 -0300

    Refactoring of bootstraped data

--- src/main/java/com/sccon/exercicio/bootstrap/BootstrapData.java
+++ src/main/java/com/sccon/exercicio/bootstrap/BootstrapData.java
@@ -20,8 +20,8 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Person jose = new Person(1L, "José da Silva", LocalDate.of(2000, 4, 6), LocalDate.of(2020, 5, 10));
-        Person joao = new Person(2L, "João dos Santos", LocalDate.of(2001, 5, 1), LocalDate.of(2021, 6, 1));
-        Person jeremias = new Person(3L, "Jeremias de Jesus",LocalDate.of(2001, 7, 1), LocalDate.of(2022, 5, 1));
+        Person joao = new Person(2L, "Bruno dos Santos", LocalDate.of(2001, 5, 1), LocalDate.of(2021, 6, 1));
+        Person jeremias = new Person(3L, "Alice de Jesus",LocalDate.of(2001, 7, 1), LocalDate.of(2022, 5, 1));
         personRepository.saveAll(Arrays.asList(jose, joao, jeremias));
     }
 }
